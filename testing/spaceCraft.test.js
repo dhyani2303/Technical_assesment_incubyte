e5bae35105e41e22523439699992977f26f760a2
Dhyani <dhyani947@gmail.com>
2023-09-12 14:55:13 +0530

    Added Complete Command

--- testing/spaceCraft.test.js
+++ testing/spaceCraft.test.js
@@ -17,3 +17,39 @@ test("for the only one command", () => {
     expect(output).toStrictEqual(expected);
   
   });
+
+
+  test("for complete Command", () => {
+    const commands = ["f", "f", "u", "l", "l"];
+  
+    const start = {
+      coordinates: [0, 0, 0],
+      direction: "W",
+    };
+  
+    const expected = {
+      coordinates: [-2, 0, 0],
+      direction: "S",
+    };
+  
+    const output = spaceCraft(commands, start);
+    expect(output).toStrictEqual(expected);
+  
+  });
+
+  test("for complete commands", () => {
+    const commands = ["f", "f", "f", "f", "f"];
+  
+    const start = {
+      coordinates: [5, -2, 1],
+      direction: "E",
+    };
+    const expected = {
+      coordinates: [10, -2, 1],
+      direction: "E",
+    };
+  
+    const output = spaceCraft(commands, start);
+    expect(output).toStrictEqual(expected);
+    
+  });
